630ae2fbe95d4fae12202c01357dc04ea6980486
Crypto1-dev <empeyjj@icloud.com>
2025-04-20 14:12:29 -0500

    Create requirements.cc

---
+++ requirements/requirements.cc
@@ -0,0 +1,9 @@
+#use main
+
+#r = GitHub repo
+
+git = https://github.com/Crypto1-dev/Pico-coin
+ 
+fish.git //your welcome, dependencies will auto install without me having to update!!!
+
+main.end()
